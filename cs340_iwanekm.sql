db1de5bb303b787414e723791244c7602a7bf183
TravisH18 <travish1842@gmail.com>
2023-02-07 11:00:49 -0800

    Updated SQL file, sample data complete.

--- cs340_iwanekm.sql
+++ cs340_iwanekm.sql
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `Astronauts` (
-  `id_astronaut` int(11) NOT NULL,
+  `id_astronaut` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `name` varchar(45) NOT NULL,
-  `age` decimal(19,2) DEFAULT NULL,
+  `age` int DEFAULT NULL,
   `gender` varchar(45) DEFAULT NULL,
   `mission_role` varchar(45) DEFAULT NULL,
   `Spacecraft_id_spacecraft` int(11) DEFAULT NULL
@@ -40,8 +40,13 @@ CREATE TABLE `Astronauts` (
 -- Dumping data for table `Astronauts`
 --
 
-INSERT INTO `Astronauts` (`id_astronaut`, `name`, `age`, `gender`, `mission_role`, `Spacecraft_id_spacecraft`) VALUES
-(1, 'Buzz Aldrin', '93.00', 'Male', 'Pilot', NULL);
+INSERT INTO `Astronauts` (`name`, `age`, `gender`, `mission_role`, `Spacecraft_id_spacecraft`)
+VALUES
+('Buzz Aldrin', 93, 'Male', 'Pilot', NULL),
+('Joe Acaba', 55, 'Male', 'Engineer', NULL),
+('Kayla Barron', 35, 'Female', 'Specialist', NULL),
+('Stephen Bowen', 58, 'Male', 'Specialist', NULL),
+('Tracy Dyson',53, 'Female', 'Specialist', NULL);
 
 -- --------------------------------------------------------
 
@@ -50,19 +55,24 @@ INSERT INTO `Astronauts` (`id_astronaut`, `name`, `age`, `gender`, `mission_role
 --
 
 CREATE TABLE `Clients` (
-  `id_client` int(11) NOT NULL,
+  `id_client` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `EIN` double DEFAULT NULL,
   `name` varchar(45) NOT NULL,
   `contribution_amount` decimal(19,2) DEFAULT NULL,
-  `address` varchar(45) DEFAULT NULL
+  `address` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 --
 -- Dumping data for table `Clients`
 --
 
-INSERT INTO `Clients` (`id_client`, `EIN`, `name`, `contribution_amount`, `address`) VALUES
-(1, 967657565, 'Lockheed Martin', '52300300.00', '3251 Hanover St, Palo Alto, CA 94304');
+INSERT INTO `Clients` (`EIN`, `name`, `contribution_amount`, `address`)
+VALUES
+(521610424, 'Lockheed Martin', 52300300.00, '3251 Hanover St, Palo Alto, CA 94304'),
+(133243459, 'Airbus', 99800000.00, '1855 Innovation Blvd. Wichita, KS 67208 - U.S.A.'),
+(910425694, 'Boeing', 677000000.00, '19000 NE Sandy Blvd, Portland, OR 97230'),
+(912166281, 'Millennium Space Systems', 64000000.00, '2265 E El Segundo Blvd, El Segundo, CA 90245'),
+(951055798, 'Northrop Grumman', 32500000.00, '2980 Fairview Park Drive, Falls Church, VA 22042, U.S.A.');
 
 -- --------------------------------------------------------
 
@@ -71,13 +81,13 @@ INSERT INTO `Clients` (`id_client`, `EIN`, `name`, `contribution_amount`, `addre
 --
 
 CREATE TABLE `Missions` (
-  `id_mission` int(11) NOT NULL,
+  `id_mission` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `name` varchar(45) NOT NULL,
   `contract_revenues` decimal(19,2) DEFAULT NULL,
   `contract_costs` decimal(19,2) DEFAULT NULL,
   `contract_profit` decimal(19,2) GENERATED ALWAYS AS (`contract_revenues` - `contract_costs`) STORED,
   `isExternal` tinyint(4) DEFAULT NULL,
-  `mission_description` varchar(300) DEFAULT NULL,
+  `mission_description` varchar(255) DEFAULT NULL,
   `Spacecraft_id_spacecraft` int(11) DEFAULT NULL,
   `Clients_id_client` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
@@ -86,9 +96,12 @@ CREATE TABLE `Missions` (
 -- Dumping data for table `Missions`
 --
 
-INSERT INTO `Missions` (`id_mission`, `name`, `contract_revenues`, `contract_costs`, `isExternal`, `mission_description`, `Spacecraft_id_spacecraft`, `Clients_id_client`) VALUES
-(1, 'Garmin GPS Network Launch Service', '152300000.00', '37520000.00', 1, 'A mission to launch a satellite for the Garmin GPS network into a geosynchronous orbit around Earth.  ', NULL, NULL);
-
+INSERT INTO `Missions` (`name`, `contract_revenues`, `contract_costs`, `isExternal`, `mission_description`, `Spacecraft_id_spacecraft`, `Clients_id_client`) VALUES
+('Garmin GPS Network Launch Service', 152300000.00, 37520000.00, 1, 'A mission to launch a satellite for the Garmin GPS network into a geosynchronous orbit around Earth.  ', NULL, NULL),
+('Space Sentry System', 5000000.00, 3450000.00, 1, 'A mission to lauch multiple low orbit defense satellites.', NULL, NULL),
+('Polar Icecap Observatory', 450000000.00, 92300000.00, 1, 'Track and Survey ice levels at the north pole.', NULL, NULL),
+('Jupiter Ultraviolet Exploration', 799000000.00, 487000000.00, 1, 'A mission with the goal of capturing the most detailed ultraviolet data from jupiter ever.',NULL, NULL),
+('Universal Iodine Detection', 1000000.00, 7500000.00, 0, 'A mission to deploy a new space telescope that specifically detects iodine.', NULL, NULL);
 -- --------------------------------------------------------
 
 --
@@ -96,26 +109,26 @@ INSERT INTO `Missions` (`id_mission`, `name`, `contract_revenues`, `contract_cos
 --
 
 CREATE TABLE `Parts` (
-  `id_part` int(11) NOT NULL,
+  `id_part` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `name` varchar(100) NOT NULL,
   `manufacturer` varchar(45) NOT NULL,
   `mass_kg` decimal(19,2) NOT NULL,
   `cost` decimal(19,2) NOT NULL,
-  `part_description` varchar(300) DEFAULT NULL
+  `part_description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 --
 -- Dumping data for table `Parts`
 --
 
-INSERT INTO `Parts` (`id_part`, `name`, `manufacturer`, `mass_kg`, `cost`, `part_description`) VALUES
-(1, 'Roll Out Solar Array (ROSA)', 'Redwire', '325.00', '17000000.00', '120 KW solar array that is 20 percent lighter and 1/4th the volume of rigid panel arrays.\r\n\r\nhttps://spaceflightnow.com/2022/12/03/iss-eva-82/#:~:text=NASA%20is%20upgrading%20the%20space,station\'s%20eight%20original%20solar%20panels.'),
-(2, 'RDR 68 Reaction Wheel', 'Collins Aerospace', '8.00', '50000.00', 'The wheels accommodate the requirements of attitude control systems for spacecraft weighing between 30 kg and 7,000 kg.\r\n\r\nhttps://www.satcatalog.com/component/rdr-68-3/'),
-(3, 'PEPT-200 Diaphragm Propellant Tank', 'Rafael Advanced Defense Systems', '0.91', '2000.00', 'Spherical tank used to store monopropellant (Krypton/Hydrazine) for RCS thrusters. \r\n\r\nhttps://www.satcatalog.com/component/pept-200-diaphragm-propellant-tank/'),
-(4, 'R-800 Hall Effect Thruster (HET)', 'Rafael Space Propulsion', '1.50', '100000.00', 'A Xenon/Krypton ion thruster for low mass, less than 1000kg, satellites.  Produces 23-50 mN (Millinewtons) of thrust.'),
-(5, 'Smallsat Perimeter Truss (SPT) Reflector ', 'L3Harris', '18.60', '75000.00', 'HCR is a high-accuracy, large aperture antenna optimized accurate enough for high\r\nfrequency missions (e.g. Ka or V band). \r\n https://www.l3harris.com/sites/default/files/2020-09/l3harris-smallsat-perimeter-truss-spec-sheet-sas.pdf'),
-(6, 'Crew Dragon Manned Capsule', 'SpaceX', '6000.00', '100000000.00', 'A manned space capsule capable of carrying 3,307 kg or 7,291 lbs of cargo to the ISS, and up to 4 astronauts. '),
-(7, 'LIFE (Large Integrated Flexible Environment) ', 'Sierra Space', '5000.00', '40000000.00', 'Inflatable habitat that expands in-orbit to a large structure that is three stories tall, and 27 feet in diameter. \r\n\r\nhttps://www.sierraspace.com/space-destinations/life-space-habitat/');
+INSERT INTO `Parts` (`name`, `manufacturer`, `mass_kg`, `cost`, `part_description`) VALUES
+('Roll Out Solar Array (ROSA)', 'Redwire', 325.00, 17000000.00, "120 KW solar array that is 20 percent lighter and 1/4th the volume of rigid panel arrays.\r\n\r\nhttps://spaceflightnow.com/2022/12/03/iss-eva-82/#:~:text=NASA%20is%20upgrading%20the%20space,station\'s%20eight%20original%20solar%20panels."),
+('RDR 68 Reaction Wheel', 'Collins Aerospace', 8.00, 50000.00, 'The wheels accommodate the requirements of attitude control systems for spacecraft weighing between 30 kg and 7,000 kg.\r\n\r\nhttps://www.satcatalog.com/component/rdr-68-3/'),
+('PEPT-200 Diaphragm Propellant Tank', 'Rafael Advanced Defense Systems', 0.91, 2000.00, 'Spherical tank used to store monopropellant (Krypton/Hydrazine) for RCS thrusters. \r\n\r\nhttps://www.satcatalog.com/component/pept-200-diaphragm-propellant-tank/'),
+('R-800 Hall Effect Thruster (HET)', 'Rafael Space Propulsion', 1.50, 100000.00, 'A Xenon/Krypton ion thruster for low mass, less than 1000kg, satellites.  Produces 23-50 mN (Millinewtons) of thrust.'),
+('Smallsat Perimeter Truss (SPT) Reflector ', 'L3Harris', 18.60, 75000.00, 'HCR is a high-accuracy, large aperture antenna optimized accurate enough for high\r\nfrequency missions (e.g. Ka or V band). \r\n https://www.l3harris.com/sites/default/files/2020-09/l3harris-smallsat-perimeter-truss-spec-sheet-sas.pdf'),
+('Crew Dragon Manned Capsule', 'SpaceX', 6000.00, 100000000.00, 'A manned space capsule capable of carrying 3,307 kg or 7,291 lbs of cargo to the ISS, and up to 4 astronauts. '),
+('LIFE (Large Integrated Flexible Environment) ', 'Sierra Space', 5000.00, 40000000.00, 'Inflatable habitat that expands in-orbit to a large structure that is three stories tall, and 27 feet in diameter. \r\n\r\nhttps://www.sierraspace.com/space-destinations/life-space-habitat/');
 
 -- --------------------------------------------------------
 
@@ -124,7 +137,7 @@ INSERT INTO `Parts` (`id_part`, `name`, `manufacturer`, `mass_kg`, `cost`, `part
 --
 
 CREATE TABLE `Planetary_Objects` (
-  `id_planetary_object` int(11) NOT NULL,
+  `id_planetary_object` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `name` varchar(45) NOT NULL,
   `surface_gravity_g` decimal(19,2) NOT NULL,
   `avg_distance_from_sun_au` decimal(19,2) NOT NULL,
@@ -136,16 +149,16 @@ CREATE TABLE `Planetary_Objects` (
 -- Dumping data for table `Planetary_Objects`
 --
 
-INSERT INTO `Planetary_Objects` (`id_planetary_object`, `name`, `surface_gravity_g`, `avg_distance_from_sun_au`, `isPlanet`, `isMoon`) VALUES
-(1, 'Mercury', '0.38', '0.39', 1, 0),
-(2, 'Venus', '0.91', '0.72', 1, 0),
-(3, 'Earth', '1.00', '1.00', 1, 0),
-(4, 'Mars', '0.38', '1.52', 1, 0),
-(5, 'Jupiter', '2.36', '5.20', 1, 0),
-(6, 'Saturn', '0.92', '9.57', 1, 0),
-(7, 'Uranus', '0.89', '19.17', 1, 0),
-(8, 'Neptune', '1.12', '30.18', 1, 0),
-(9, 'Pluto', '0.07', '39.48', 0, 0);
+INSERT INTO `Planetary_Objects` (`name`, `surface_gravity_g`, `avg_distance_from_sun_au`, `isPlanet`, `isMoon`) VALUES
+('Mercury', 0.38, 0.39, 1, 0),
+('Venus', 0.91, 0.72, 1, 0),
+('Earth', 1.00, 1.00, 1, 0),
+('Mars', 0.38, 1.52, 1, 0),
+('Jupiter', 2.36, 5.20, 1, 0),
+('Saturn', 0.92, 9.57, 1, 0),
+('Uranus', 0.89, 19.17, 1, 0),
+('Neptune', 1.12, 30.18, 1, 0),
+('Pluto', 0.07, 39.48, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -154,21 +167,25 @@ INSERT INTO `Planetary_Objects` (`id_planetary_object`, `name`, `surface_gravity
 --
 
 CREATE TABLE `Spacecraft` (
-  `id_spacecraft` int(11) NOT NULL,
+  `id_spacecraft` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `name` varchar(45) NOT NULL,
   `in_orbit` tinyint(4) DEFAULT NULL,
   `launched` tinyint(4) DEFAULT NULL,
   `Planetary_Objects_id_planetary_object` int(11) NOT NULL,
-  `delta_v_remaining` decimal(19,2) DEFAULT NULL,
-  `mission_elapsed_time_days` decimal(19,2) DEFAULT NULL
+  `delta_v_remaining` int DEFAULT NULL,
+  `mission_elapsed_time_days` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 --
 -- Dumping data for table `Spacecraft`
 --
 
-INSERT INTO `Spacecraft` (`id_spacecraft`, `name`, `in_orbit`, `launched`, `Planetary_Objects_id_planetary_object`, `delta_v_remaining`, `mission_elapsed_time_days`) VALUES
-(1, 'Garmin GPS Satellite 34', 1, 1, 3, '500.00', '43.00');
+INSERT INTO `Spacecraft` (`name`, `in_orbit`, `launched`, `Planetary_Objects_id_planetary_object`, `delta_v_remaining`, `mission_elapsed_time_days`) VALUES
+('Garmin GPS Satellite 34', 1, 1, 3, 500, 43),
+('Artic Glacier Satellite 02', 1, 1, 3, 240, 105),
+('Sentry', 0, 0, 4, 600, 21),
+('Extraterrestrial Ultraviolet Explorer', 0, 0, 5, 100, 212),
+('Iodine Space Telescope', 0, 1, 8, 355, 149);
 
 -- --------------------------------------------------------
 
@@ -199,7 +216,6 @@ INSERT INTO `Spacecraft_has_Parts` (`Spacecraft_id_spacecraft`, `Parts_id_part`)
 -- Indexes for table `Astronauts`
 --
 ALTER TABLE `Astronauts`
-  ADD PRIMARY KEY (`id_astronaut`),
   ADD UNIQUE KEY `id_astronaut_UNIQUE` (`id_astronaut`),
   ADD KEY `fk_Astronauts_Spacecraft1_idx` (`Spacecraft_id_spacecraft`);
 
@@ -207,14 +223,12 @@ ALTER TABLE `Astronauts`
 -- Indexes for table `Clients`
 --
 ALTER TABLE `Clients`
-  ADD PRIMARY KEY (`id_client`),
   ADD UNIQUE KEY `id_client_UNIQUE` (`id_client`);
 
 --
 -- Indexes for table `Missions`
 --
 ALTER TABLE `Missions`
-  ADD PRIMARY KEY (`id_mission`),
   ADD UNIQUE KEY `id_mission_UNIQUE` (`id_mission`),
   ADD UNIQUE KEY `name_UNIQUE` (`name`),
   ADD KEY `fk_Missions_Spacecraft1_idx` (`Spacecraft_id_spacecraft`),
@@ -224,21 +238,18 @@ ALTER TABLE `Missions`
 -- Indexes for table `Parts`
 --
 ALTER TABLE `Parts`
-  ADD PRIMARY KEY (`id_part`),
   ADD UNIQUE KEY `id_part_UNIQUE` (`id_part`);
 
 --
 -- Indexes for table `Planetary_Objects`
 --
 ALTER TABLE `Planetary_Objects`
-  ADD PRIMARY KEY (`id_planetary_object`),
   ADD UNIQUE KEY `id_planetary_object_UNIQUE` (`id_planetary_object`);
 
 --
 -- Indexes for table `Spacecraft`
 --
 ALTER TABLE `Spacecraft`
-  ADD PRIMARY KEY (`id_spacecraft`,`Planetary_Objects_id_planetary_object`),
   ADD UNIQUE KEY `id_spacecraft_UNIQUE` (`id_spacecraft`),
   ADD KEY `fk_Spacecraft_Planetary_Objects1_idx` (`Planetary_Objects_id_planetary_object`);
 
@@ -246,49 +257,8 @@ ALTER TABLE `Spacecraft`
 -- Indexes for table `Spacecraft_has_Parts`
 --
 ALTER TABLE `Spacecraft_has_Parts`
-  ADD PRIMARY KEY (`Spacecraft_id_spacecraft`,`Parts_id_part`),
   ADD KEY `fk_Spacecraft_has_Parts_Parts1_idx` (`Parts_id_part`),
   ADD KEY `fk_Spacecraft_has_Parts_Spacecraft1_idx` (`Spacecraft_id_spacecraft`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Astronauts`
---
-ALTER TABLE `Astronauts`
-  MODIFY `id_astronaut` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `Clients`
---
-ALTER TABLE `Clients`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `Missions`
---
-ALTER TABLE `Missions`
-  MODIFY `id_mission` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `Parts`
---
-ALTER TABLE `Parts`
-  MODIFY `id_part` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `Planetary_Objects`
---
-ALTER TABLE `Planetary_Objects`
-  MODIFY `id_planetary_object` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `Spacecraft`
---
-ALTER TABLE `Spacecraft`
-  MODIFY `id_spacecraft` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
